03192c2051a0ba9a96318f43021d03a652e9865e
BangBungKop <115243943+BangBungKop@users.noreply.github.com>
2022-10-07 09:45:50 -0700

    Update renew-ws.sh

--- xray/renew-ws.sh
+++ xray/renew-ws.sh
@@ -4,7 +4,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/MyRidwan/izinvps/ipuk/ip > /root/tmp
+    curl -sS https://raw.githubusercontent.com/BangBungKop/izinbang/main/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
